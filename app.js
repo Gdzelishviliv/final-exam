1a3db781fbf7c165489407485070d0ce13f52a21
ivane <gdzelishviliv@gmail.com>
2024-01-07 15:24:40 +0400

    added day/night btn with js

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const dayNightBtn = document.querySelector('.day-night-btn');
+
+    dayNightBtn.addEventListener('click', function () {
+        document.body.classList.toggle('day-mode');
+    });
+});
